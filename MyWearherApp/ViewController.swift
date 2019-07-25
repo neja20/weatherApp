02ee9654be3c8f722675e0ef285e71c354b8bd6d
Daniel Nejezchleb <xnejd00@vse.cz>
2019-07-25 13:00:01 +0200

    Initial Commit

---
+++ MyWearherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWearherApp
+//
+//  Created by D N on 25/07/2019.
+//  Copyright © 2019 Daniel Nejezchleb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
